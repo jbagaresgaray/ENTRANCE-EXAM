52ed0219a898b654d8b840ba6945384dfaea9b78
Philip Cesar <philipgaray2@gmail.com>
2015-08-04 02:03:35 +0800

    update category API

--- server/category/index.php
+++ server/category/index.php
@@ -1,32 +1,44 @@
 <?php
 	include('../../server/cors.php');
-	include( __DIR__.'/controller.php'); 
+	include( __DIR__.'/controller.php');
 
-	switch ($_POST['command']) {
-		case 'create_category':
-			$data = [
-				"category_name" => $_POST['category_name']
-			];
-			Category::create($data);
-			break;
-		case 'read_category':
-			$search = isset($_POST['search']) ? $_POST['search']: null;
+	$method = $_SERVER['REQUEST_METHOD'];
+	$request = explode("/", substr(@$_SERVER['PATH_INFO'], 1));
+
+	switch ($method) {
+	  case 'PUT':
+  		$data = [
+			"category_name" => $_POST['category_name'],
+			"id" => $request
+		];
+		Category::update($data);
+	    break;
+	  case 'POST':
+	    $data = [
+			"category_name" => $_POST['category_name']
+		];
+		Category::create($data);
+	    break;
+	  case 'GET':
+	  	if(isset($request) && !empty($request)){
+	  		print json_encode($request);
+	  		// Category::detail($request);
+	  	}else{
+	  		$search = isset($_POST['search']) ? $_POST['search']: null;
 			$page = isset($_POST['page']) ? $_POST['page']: 1;
 			Category::read($page,$search);
-			break;
-		case 'update_category':
-			$data = [
-				"category_name" => $_POST['category_name'],
-				"id" => $_POST['id']
-			];
-			Category::update($data);
-			break;
-		case 'delete_category':
-			Category::delete($_POST['id']);
-			break;
-		default:	
-			echo 'ENTRANCE EXAM API v.0.1 developed by: Philip Cesar B. Garay';
-			break;
+	  	}
+	    break;
+	  case 'DELETE':
+	  	if(isset($request) && !empty($request)){
+	  		Category::delete($request);
+	  	}else{
+	  		Category::delete($_POST['id']);
+	  	}	   
+	    break;
+	  default:
+	    print json_encode('ENTRANCE EXAM API v.0.1 developed by: Philip Cesar B. Garay');
+	    break;
 	}
 	exit();
 	
